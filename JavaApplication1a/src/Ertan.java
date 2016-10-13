dba5f4042aa8fc54fe4130d71bb7a746e30ce9cf
ertangokyildirim <ertan_gokyildirim@hotmail.com>
2016-10-13 15:28:18 +0300

    class added

---
+++ JavaApplication1a/src/Ertan.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 213CS2212
+ */
+public class Ertan {
+    
+}
