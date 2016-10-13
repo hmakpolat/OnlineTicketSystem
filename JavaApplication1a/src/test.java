aa00e071926e6576a440802a596c8745dc18f7a0
ertangokyildirim <ertan_gokyildirim@hotmail.com>
2016-10-13 16:06:12 +0300

    test class

---
+++ JavaApplication1a/src/test.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 213CS2212
+ */
+public class test {
+    public static void main(String[] args) {
+        int a = 8;
+    }
+}
